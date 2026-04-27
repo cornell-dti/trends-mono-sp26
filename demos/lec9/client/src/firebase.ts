814fc26f4a43c30c993b6af9248357c1870c407f
Simon Ilincev <trilogicworlds@gmail.com>
2026-04-27 15:55:41 -0400

    chore: update Firebase configuration for demo7

--- demos/lec9/client/src/firebase.ts
+++ demos/lec9/client/src/firebase.ts
@@ -4,13 +4,14 @@ import { getAuth } from "firebase/auth";
 
 // Our web app's Firebase configuration (example)
 const firebaseConfig = {
-  apiKey: "AIzaSyAp-r5EOre-s8pZm3eHDrag61Mx8-yNP0w",
-  authDomain: "trends-fa25-lec8.firebaseapp.com",
-  projectId: "trends-fa25-lec8",
-  storageBucket: "trends-fa25-lec8.firebasestorage.app",
-  messagingSenderId: "920197682301",
-  appId: "1:920197682301:web:414539ad6f47aad52fcd14"
+  apiKey: "AIzaSyBTq8T-Yb-RoFMx3cPvRQ8JqT8XsYa_5RQ",
+  authDomain: "trends-sp25-lec7-demo.firebaseapp.com",
+  projectId: "trends-sp25-lec7-demo",
+  storageBucket: "trends-sp25-lec7-demo.firebasestorage.app",
+  messagingSenderId: "1054386809202",
+  appId: "1:1054386809202:web:18521b9b9e1936ce6055eb"
 };
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
